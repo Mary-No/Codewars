3b158cc163cd873936ca41ae1c6b5c6d19d97e40
Novik_Maria <novik.work.mail@gmail.com>
2023-03-10 11:57:44 +0300

    task 7 kyu The Coupon Code

---
+++ 7 kyu The Coupon Code.js
@@ -0,0 +1,10 @@
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+  if (
+    enteredCode === correctCode &&
+    new Date(currentDate) <= new Date(expirationDate)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
